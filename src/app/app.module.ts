ba7b5d06ed80dea318e1e6bb74e93fb67004856f
josepereza <josepereza66@gmail.com>
2022-04-10 12:53:29 +0200

    first commit2

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FormularioComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+   
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
